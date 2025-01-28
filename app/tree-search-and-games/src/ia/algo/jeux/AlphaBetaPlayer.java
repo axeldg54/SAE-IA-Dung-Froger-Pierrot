de6d2c9aa5f647ad10b641547eb448a7744148c2
axeldg54 <dung1u@etu.univ-lorraine.fr>
2025-01-28 16:33:54 +0100

    merge

--- app/tree-search-and-games/src/ia/algo/jeux/AlphaBetaPlayer.java
+++ app/tree-search-and-games/src/ia/algo/jeux/AlphaBetaPlayer.java
@@ -23,11 +23,13 @@ public class AlphaBetaPlayer extends Player {
 	@Override
 	public Action getMove(GameState state) {
 		ActionValuePair bestMove;
-		
+
 		if (player == PLAYER1) {
 			bestMove = maxVal(state, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+			//bestMove = maxVal(state, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 2);
 		} else {
 			bestMove = minVal(state, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+			//bestMove = minVal(state, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 2);
 		}
 		
 		return bestMove.getAction();
@@ -112,6 +114,92 @@ public class AlphaBetaPlayer extends Player {
 			}
 		}
 		
+		return new ActionValuePair(bestAction, minValue);
+	}
+
+	/**
+	 * Fonction Minimax pour maximiser le score.
+	 * @param state
+	 * @param alpha
+	 * @param beta
+	 * @param depth Profondeur de recherche
+	 * @return
+	 */
+	public ActionValuePair maxVal(GameState state, double alpha, double beta, int depth) {
+		// Vérifie si l'état est final
+		if (state.isFinalState() || depth == 0) {
+			return new ActionValuePair(null, state.getGameValue());
+		}
+
+		double maxValue = Double.NEGATIVE_INFINITY;
+		Action bestAction = null;
+
+		// Parcourt toutes les actions possibles
+		for (Action action : game.getActions(state)) {
+			State nextState = game.doAction(state, action);
+
+			this.incStateCounter(); // compteur d'états visités
+
+			// Évalue la valeur minimale de l'adversaire
+			ActionValuePair nextActionValuePair = minVal((GameState) nextState, alpha, beta, depth - 1);
+
+			if (nextActionValuePair.getValue() >= maxValue) {
+				maxValue = nextActionValuePair.getValue();
+
+				if (maxValue > alpha) {
+					alpha = maxValue;
+					bestAction = action;
+				}
+			}
+
+			if (maxValue >= beta) {
+				return new ActionValuePair(bestAction, maxValue);
+			}
+		}
+
+		return new ActionValuePair(bestAction, maxValue);
+	}
+
+	/**
+	 * Fonction Minimax pour minimiser le score.
+	 * @param state
+	 * @param alpha
+	 * @param beta
+	 * @param depth Profondeur de recherche
+	 * @return
+	 */
+	public ActionValuePair minVal(GameState state, double alpha, double beta, int depth) {
+		// Vérifie si l'état est final
+		if (state.isFinalState() || depth == 0) {
+			return new ActionValuePair(null, state.getGameValue());
+		}
+
+		double minValue = Double.POSITIVE_INFINITY;
+		Action bestAction = null;
+
+		// Parcourt toutes les actions possibles
+		for (Action action : game.getActions(state)) {
+			State nextState = game.doAction(state, action);
+
+			this.incStateCounter(); // compteur d'états visités
+
+			// Évalue la valeur maximale de l'adversaire
+			ActionValuePair nextActionValuePair = maxVal((GameState) nextState, alpha, beta, depth - 1);
+
+			if (nextActionValuePair.getValue() <= minValue) {
+				minValue = nextActionValuePair.getValue();
+
+				if (minValue < beta) {
+					beta = minValue;
+					bestAction = action;
+				}
+			}
+
+			if (minValue <= alpha) {
+				return new ActionValuePair(bestAction, minValue);
+			}
+		}
+
 		return new ActionValuePair(bestAction, minValue);
 	}
 }
